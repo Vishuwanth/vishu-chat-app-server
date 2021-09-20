cd7330d9b52341151683d65ea6555456dcaaba2f
Vishuwanth <82795693+Vishuwanth@users.noreply.github.com>
2021-09-21 02:35:03 +0530

    make it better

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const { Server } = require('socket.io')
+app.use(cors())
+require('dotenv').config()
 
-const server = http.createServer(app);
+const port = process.env.PORT || 3001
+
+const server = http.createServer(app)
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	},
+})
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+io.on('connection', (socket) => {
+	console.log(`User Connected: ${socket.id}`)
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+	socket.on('join_room', (data) => {
+		socket.join(data)
+		console.log(`User with ID: ${socket.id} joined room: ${data}`)
+	})
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+	socket.on('send_message', (data) => {
+		socket.to(data.room).emit('receive_message', data)
+	})
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
+	socket.on('disconnect', () => {
+		console.log('User Disconnected', socket.id)
+	})
+})
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
-});
+app.get('/', (req, res) => {
+	res.send(process.env.SECRET_KEY)
+})
+
+server.listen(port, () => {
+	console.log('SERVER RUNNING')
+})
