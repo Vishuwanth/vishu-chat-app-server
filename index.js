4840c85d380056d7c728233b13d283ccf74b9e35
Vishuwanth <82795693+Vishuwanth@users.noreply.github.com>
2021-09-21 02:49:40 +0530

    asd

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
 	cors: {
-		origin: 'https://agitated-shockley-2ae6d1.netlify.app/',
+		origin: '*',
 		methods: ['GET', 'POST'],
 	},
 })
